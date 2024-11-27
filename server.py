7a84fce11e7effca533e83f33edf78165b95a67a
Lanxtot <80767096+Lanxtot@users.noreply.github.com>
2024-11-27 08:41:35 +0200

    Update server.py

--- server.py
+++ server.py
@@ -15,7 +15,11 @@ def product():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-    
+
+@app.route('/volvo1')
+def volvo1():
+    return render_template('volvo1.jpg')
+
 # Paleidžiame serverį
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
